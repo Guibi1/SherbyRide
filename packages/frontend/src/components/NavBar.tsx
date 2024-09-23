d27cfcc10363f3550863a1c9ca3143cc6a17731b
Laurent Stéphenne <laurent@guibi.dev>
2024-09-23 15:11:04 -0400

    Navbar with user

--- packages/frontend/src/components/NavBar.tsx
+++ packages/frontend/src/components/NavBar.tsx
@@ -1,37 +1,26 @@
 import { auth, signIn, signOut } from "@/lib/auth";
 import { CarIcon } from "lucide-react";
 import Link from "next/link";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default async function NavBar() {
-    const session = await auth();
+    const user = (await auth())?.user;
 
     return (
-        <header className="px-4 lg:px-6 h-14 flex items-center border-b">
+        <header className="px-4 lg:px-6 h-14 flex items-center border-b gap-8">
             <Link className="flex items-center justify-center" href="/">
                 <CarIcon className="h-6 w-6" />
                 <span className="ml-2 text-lg font-semibold">SherbyRide</span>
             </Link>
-
-            {session ? (
-                <form
-                    action={async () => {
-                        "use server";
-                        await signOut();
-                    }}
-                >
-                    <Button type="submit">Log out</Button>
-                </form>
-            ) : (
-                <form
-                    action={async () => {
-                        "use server";
-                        await signIn("keycloak");
-                    }}
-                >
-                    <Button type="submit">Login</Button>
-                </form>
-            )}
 
             <nav className="ml-auto flex gap-4 sm:gap-6">
                 <Link href="/rides" className="text-sm font-medium hover:underline underline-offset-4">
@@ -46,6 +35,53 @@ export default async function NavBar() {
                     How It Works
                 </Link>
             </nav>
+
+            {user ? (
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Avatar className="border">
+                            <AvatarImage src={user?.image || undefined} />
+                            <AvatarFallback>{user?.name?.at(0)}</AvatarFallback>
+                        </Avatar>
+                    </DropdownMenuTrigger>
+
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>
+                            {user?.name}
+                            <div className="text-muted-foreground font-light text-sm">{user?.email}</div>
+                        </DropdownMenuLabel>
+
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem asChild>
+                            <Link href="/profile">Mon profil</Link>
+                        </DropdownMenuItem>
+                        <DropdownMenuItem asChild>
+                            <Link href="/rides">Mes rides</Link>
+                        </DropdownMenuItem>
+
+                        <DropdownMenuSeparator />
+                        <form
+                            action={async () => {
+                                "use server";
+                                await signOut();
+                            }}
+                        >
+                            <DropdownMenuItem className="text-destructive w-full" asChild>
+                                <button type="submit">Se déconnecter</button>
+                            </DropdownMenuItem>
+                        </form>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            ) : (
+                <form
+                    action={async () => {
+                        "use server";
+                        await signIn("keycloak");
+                    }}
+                >
+                    <Button type="submit">Login</Button>
+                </form>
+            )}
         </header>
     );
 }
