8953ad80ae35f85b67fab0f6d40b4ab9b5ee7ebd
Noky121 <frae2001@usherbrooke.ca>
2024-09-25 21:54:17 -0400

    Sprint 1

---
+++ packages/frontend/src/app/offers/TrajetCreationForm.tsx
@@ -0,0 +1,136 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import type { User } from "@/lib/auth";
+import { type Faculty, faculties } from "@/lib/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { getSession } from "next-auth/react";
+import { redirect, useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { parse, parseISO } from "date-fns";
+import { z } from "zod";
+
+const formSchema = z.object({
+    depart: z.string().max(50),
+    arrive: z.string().max(50),
+    date: z.string(),
+    passagers: z.number().min(1),
+});
+
+export default function TrajetCreationForm({ user }: { user: User }) {
+    const router = useRouter();
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            depart: "",
+            arrive: "",
+            date: "",
+            passagers: 0,
+        },
+    });
+
+    async function onSubmit(values: z.infer<typeof formSchema>) {
+        const session = await getSession();
+        const res = await fetch("http://localhost:8080/trajet", {
+            method: "POST",
+            headers: { Authorization: `Bearer ${session?.accessToken}`, "Content-Type": "application/json" },
+            body: JSON.stringify({
+                driver: user.cip,
+                departureLoc: values.depart,
+                arrivalLoc: values.arrive,
+                departureTime: values.date,
+                maxPassagers: values.passagers,
+            }),
+        });
+
+        if (res.ok) {
+            router.refresh();
+        }
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-8 container">
+                <FormField
+                    control={form.control}
+                    name="depart"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Lieu de départ</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="arrive"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Destination</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="date"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Date de départ</FormLabel>
+                            <FormControl>
+                                <Input {...field} type="datetime-local" />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <FormField
+                    control={form.control}
+                    name="passagers"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Combien de passagers pouvez-vous apporter?</FormLabel>
+                            <FormControl>
+                                <Select
+                                    name={field.name}
+                                    value={field.value.toString()}
+                                    disabled={field.disabled}
+                                    onValueChange={(v) => field.onChange(+v)}
+                                    onOpenChange={field.onBlur}
+                                >
+                                    <SelectTrigger>
+                                        <SelectValue />
+                                    </SelectTrigger>
+
+                                    <SelectContent>
+                                        <SelectItem value="1">1</SelectItem>
+                                        <SelectItem value="2">2</SelectItem>
+                                        <SelectItem value="3">3</SelectItem>
+                                        <SelectItem value="4">4</SelectItem>
+                                        <SelectItem value="5">5</SelectItem>
+                                        <SelectItem value="6">6</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                <Button type="submit">Publier l'offre</Button>
+            </form>
+        </Form>
+    );
+}
