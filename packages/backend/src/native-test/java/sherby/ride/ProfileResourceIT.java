25309e353476bbe5fec836428382465034a1ea24
Laurent Stéphenne <laurent@guibi.dev>
2024-11-21 10:15:39 -0500

    Fix

--- packages/backend/src/native-test/java/sherby/ride/ProfileResourceIT.java
+++ packages/backend/src/native-test/java/sherby/ride/ProfileResourceIT.java
@@ -1,9 +1,8 @@
 package sherby.ride;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import sherby.ride.GreetingResourceTest;
 
 @QuarkusIntegrationTest
-class GreetingResourceIT extends GreetingResourceTest {
+class ProfileResourceIT extends ProfileResourceTest {
     // Execute the same tests but in packaged mode.
 }
